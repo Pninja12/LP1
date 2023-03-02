99f3e619112ccac0ea242ff6c7986c9714df2d5c
“Pninja12” <pauloalmeida1288@gmail.com>
2023-03-02 23:27:10 +0000

    Second try

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -7,9 +7,9 @@ namespace Argumentos
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            for(int i = 0; i < 10; i++):
+            for(int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("%d ", i);
             }
         }
     }
