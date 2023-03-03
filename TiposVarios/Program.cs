046069d87c20b1f32e47a9d41eb616c40edf444a
Paulo Silva <a22206205@alunos.ulht.pt>
2023-03-03 11:00:53 +0000

    TiposVarios 2º point made

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 0;
+            int b = 1;
+            int c = 2;
+
+            Console.WriteLine("{0}", a);
+            Console.WriteLine("{0} {1}", b, c);
+        }
+    }
+}
+
