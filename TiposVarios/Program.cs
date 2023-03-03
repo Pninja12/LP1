3926e55453f251a0dc05e16d84be5329f080ab07
Paulo Silva <a22206205@alunos.ulht.pt>
2023-03-03 11:22:25 +0000

    TiposVarios 3º point made

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,9 +10,11 @@ namespace TiposVarios
             int a = 0;
             int b = 1;
             int c = 2;
+            char unicode = '\u4327';
 
             Console.WriteLine("{0}", a);
             Console.WriteLine("{0} {1}", b, c);
+            Console.WriteLine(unicode);
         }
     }
 }
